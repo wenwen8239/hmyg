274f90dde005b2e89d7be60dcde8bb3f7fb0444b
陈静雯 <917258239@qq.com>
2019-08-06 19:37:51 +0800

    实现订单页面

--- pages/order/index.js
+++ pages/order/index.js
@@ -2,6 +2,8 @@
 import { getStorageToken } from '../../utils/storage';
 // 引入封装好的请求数据的js文件
 import { request } from '../../request/index.js';
+// 支持es7的async语法
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
   data: {
     tabs: [
@@ -18,17 +20,23 @@ Page({
     // console.log(e)
     // 获取子组件传递的id值
     const {index} = e.detail;
+    // 调用根据索引切换标题函数
+    this.changeTitleByIndex(index);
+    // 获取当前点击标题的type
+    let type = index + 1;
+    // 重新发送请求
+    this.getOrderList(type);
+  },
+  // 根据索引切换标题
+  changeTitleByIndex(index) {
     // 获取tabs栏数据
     const {tabs} = this.data;
     // 把当前点击的tabs的isActive属性修改为true
     tabs.forEach((e,i) => i === index ? e.isActive = true : e.isActive = false);
-    // 把数据重新修改会data中
+    // 把数据重新修改回data中
     this.setData({
       tabs
     })
-  },
-  onLoad: function (options) {
-
   },
   onShow() {
     // 从本地存储中获取token 
@@ -41,15 +49,35 @@ Page({
       });
       return;
     }
+    // 获取当前页面栈数组
     let pageList = getCurrentPages();
-    console.log(pageList)
+    // 获取当前页面的参数
     let currentPage = pageList[pageList.length - 1];
     // 获取传递过来的type属性
     const {type} = currentPage.options;
-    console.log(type)
+    // 调用获取所有订单数组函数
+    this.getOrderList(type)
+    // 定义要激活的索引
+    let index = type - 1;
+    // 调用根据索引切换标题函数
+    this.changeTitleByIndex(index)
   },
-  // 获取历史订单数组
-  getOrderList() {
-
+  // 封装获取历史订单数组函数
+  async getOrderList(type) {
+    // 获取用户token
+    let header = {
+      Authorization: getStorageToken()
+    }
+    // 获取所有订单信息
+    let {orders} = await request({url: '/my/orders/all', data: {type}, header: header})
+    // console.log(orders)
+    // 修改时间格式
+    orders.forEach(v => {
+      v.create_time_cn = (new Date(v.create_time * 1000)).toLocaleString();
+    })
+    // 把获取到的orders设置到data中
+    this.setData({
+      orderList: orders
+    })
   }
 })
